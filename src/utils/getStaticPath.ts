efdf9f475df98112c42365e95e831988b4b5f857
ICE0208 <doky3307@naver.com>
2023-11-26 03:19:45 +0900

    static path 수정

--- src/utils/getStaticPath.ts
+++ src/utils/getStaticPath.ts
@@ -1,6 +1,6 @@
 import { join } from 'path';
 
 export default function getStaticPath(subPath: string) {
-  const filePath = join(__dirname + `../static/${subPath}`);
+  const filePath = join(__dirname + `/../static/${subPath}`);
   return filePath;
 }

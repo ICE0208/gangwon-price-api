83eefecc7db517cb30353699bf8f33ebfb70678a
ICE0208 <doky3307@naver.com>
2023-11-26 03:16:30 +0900

    static 파일 경로 설정

--- src/utils/getStaticPath.ts
+++ src/utils/getStaticPath.ts
@@ -1,6 +1,6 @@
 import { join } from 'path';
 
 export default function getStaticPath(subPath: string) {
-  const filePath = join(`../static/${subPath}`);
+  const filePath = join(__dirname + `../static/${subPath}`);
   return filePath;
 }

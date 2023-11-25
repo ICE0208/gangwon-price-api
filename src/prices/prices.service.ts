390f118fbbcde1383a8b2378e816bb7cf4f9e7b8
ICE0208 <doky3307@naver.com>
2023-11-26 03:18:49 +0900

    static path test

--- src/prices/prices.service.ts
+++ src/prices/prices.service.ts
@@ -15,7 +15,7 @@ export class PricesService {
       const fileContent = await readFileAsync(filePath);
       return { ok: true, data: JSON.parse(fileContent.toString()) };
     } catch (error) {
-      return { ok: false, error: 'Failed to read the file' };
+      return { ok: false, error: `Failed to read the file (${filePath})` };
     }
   }
 }

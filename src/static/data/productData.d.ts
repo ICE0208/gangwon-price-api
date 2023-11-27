9fe5daf2ce0780dcc4629171d13fe0ae6940b3bb
ICE0208 <doky3307@naver.com>
2023-11-27 11:16:12 +0900

    모든 상품에 대한 fakeDB 생성 및 사용 (#3)
    * 모든 상품에 대한 fakeDB 생성 및 사용
    
    * 파일 & 코드 정리

---
+++ src/static/data/productData.d.ts
@@ -0,0 +1,23 @@
+interface IPrices {
+  충청남도: number;
+  공주시: number;
+  금산군: number;
+  논산시: number;
+  당진시: number;
+  보령시: number;
+  부여군: number;
+  서산시: number;
+  서천군: number;
+  아산시: number;
+  예산군: number;
+  천안시: number;
+  청양군: number;
+  태안군: number;
+  홍성군: number;
+  계롱시: number;
+}
+
+export interface IProduct {
+  name: string;
+  prices: IPrices;
+}

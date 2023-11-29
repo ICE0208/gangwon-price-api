63e7ef24dc7932c8c826feb3e03beca554b98927
ICE0208 <doky3307@naver.com>
2023-11-29 12:14:16 +0900

    Refactoring : 중복 코드 제거, 주석 추가 (#4)
    * 필요없는 코드 삭제
    
    * 주석을 추가함

--- src/prices/prices.service.ts
+++ src/prices/prices.service.ts
@@ -5,9 +5,10 @@ import { IProduct } from '../static/data/productData';
 
 @Injectable()
 export class PricesService {
+  // /prices?product={product} 경로를 함수입니다.
   async getLocalPrice({ product }: { product?: string }) {
-    const filePath = getStaticPath('/data/productData.json');
-
+    // product 쿼리가 주어지지 않았을 때 조건입니다.
+    // product 쿼리는 필수로 주어져야 하므로 'ok: false'와 함께 리턴합니다.
     if (product === undefined) {
       return {
         ok: false,
@@ -15,10 +16,19 @@ export class PricesService {
       };
     }
 
+    // 데이터 파일의 저장 경로입니다.
+    // src/static 아래부터 작성합니다.
+    const filePath = getStaticPath('/data/productData.json');
+
+    // 파일을 읽는 도중 에러가 발생할 수 있으므로 try-catch 문으로 감싸줍니다.
     try {
+      // 파일을 불러와 JSON으로 파싱하면 IProduct 배열 나와야합니다.
+      // 이 결과값인 배열을 fakeDB 변수에 저장합니다.
       const fileContent = await readFileAsync(filePath);
       const fakeDB = JSON.parse(fileContent.toString()) as IProduct[];
 
+      // 데이터에 product가 있는지 찾습니다.
+      // 성공 시 찾은 데이터를 리턴하고 실패하면 'ok: false'와 함께 리턴합니다.
       const productData = fakeDB.find((element) => element.name === product);
       if (productData === undefined) {
         return { ok: false, error: `${product}에 대한 상품 정보가 없습니다.` };
